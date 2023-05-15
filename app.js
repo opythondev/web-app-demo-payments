83c51f323ad28286c5155e96433ac147a5c91cb9
Sergey <21218903+xchakiex@users.noreply.github.com>
2023-05-15 19:23:30 +0300

    fix[app.js]: print lastname + id

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 p.innerText = `${tg.initDataUnsafe.user.first_name}
-{tg.initDataUnsafe.user.last_name}
-{tg.initDataUnsafe.user.id}`;
+${tg.initDataUnsafe.user.last_name}
+${tg.initDataUnsafe.user.id}`;
 
 usercard.appendChild(p);
